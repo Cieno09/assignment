67ee791517bc7ed8d622aedc6805cf53028bc222
Cieno09 <markindeisaac@gmail.com>
2023-02-06 11:12:49 +0100

    Feature 2nd commit

---
+++ SQLQuerytest.sql
@@ -0,0 +1,15 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+Select * from [SalesLT].[Address]
+Select * from [SalesLT].[Customer]
+Select * from [SalesLT].[CustomerAddress]
+Select * from [SalesLT].[Product]
+Select * from [SalesLT].[ProductDescription]
+Select * from [SalesLT].[ProductModel]
+Select * from [SalesLT].[ProductModelProductDescription]
+Select * from [SalesLT].[SalesOrderDetail]
+Select * from [SalesLT].[SalesOrderHeader]
+Select * from [SalesLT].[SalesOrderLower]
+
+
+Select * into localhost.[SalesLT].[Address]  
+from [SalesLT].[Address] 
