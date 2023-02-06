248b35277e3f90de29ebd043646815a612c7c965
Cieno09 <markindeisaac@gmail.com>
2023-02-06 11:14:29 +0100

    Feature 3rd commit

--- SQLQuerytest.sql
+++ SQLQuerytest.sql
@@ -9,6 +9,7 @@ Select * from [SalesLT].[ProductModelProductDescription]
 Select * from [SalesLT].[SalesOrderDetail]
 Select * from [SalesLT].[SalesOrderHeader]
 Select * from [SalesLT].[SalesOrderLower]
+Select * from [SalesLT].[SalesIncomeDetail]
 
 
 Select * into localhost.[SalesLT].[Address]  
